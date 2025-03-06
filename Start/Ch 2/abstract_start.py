73bd91df33cdbee19555bbc0a26c184ae5b45fb6
Anupam Siwakoti <50481498+Anupam1223@users.noreply.github.com>
2025-03-06 19:44:46 +0000

    multiple inheritance

--- Start/Ch 2/abstract_start.py
+++ Start/Ch 2/abstract_start.py
@@ -1,11 +1,13 @@
 # Python Object Oriented Programming by Joe Marini course example
 # Using Abstract Base Classes to enforce class constraints
+from math import pi, sqrt
+from abc import ABC, abstractmethod
 
-
-class GraphicShape:
+class GraphicShape(ABC):
     def __init__(self):
         super().__init__()
 
+    @abstractmethod
     def calcArea(self):
         pass
 
@@ -14,13 +16,18 @@ class Circle(GraphicShape):
     def __init__(self, radius):
         self.radius = radius
 
+    def calcArea(self):
+        return pi*sqrt(self.radius)
 
 class Square(GraphicShape):
     def __init__(self, side):
         self.side = side
 
+    def calcArea(self):
+        return sqrt(self.side)
 
-g = GraphicShape()
+#cannot instantiate abstract class
+#g = GraphicShape()
 
 c = Circle(10)
 print(c.calcArea())
