b133179b991845584cc60dd6a5b24f6b0c8ad417
Anupam Siwakoti <50481498+Anupam1223@users.noreply.github.com>
2025-03-06 19:31:30 +0000

    added class inheritance relation

--- Start/Ch 2/inheritance_start.py
+++ Start/Ch 2/inheritance_start.py
@@ -1,35 +1,30 @@
-# Python Object Oriented Programming by Joe Marini course example
-# Understanding class inheritance
-
-
-class Book:
-    def __init__(self, title, author, pages, price):
+class Baap:
+    def __init__(self, title, price):
         self.title = title
         self.price = price
+
+class Book(Baap):
+    def __init__(self, title, author, pages, price):
+        super().__init__(title, price)
         self.author = author
         self.pages = pages
 
-
-class Magazine:
+class Magazine(Baap):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
+        super().__init__(title, price)
         self.publisher = publisher
+        self.period = period
 
-
-class Newspaper:
+class Newspaper(Baap):
     def __init__(self, title, publisher, price, period):
-        self.title = title
-        self.price = price
-        self.period = period
+        super().__init__(title, price)
         self.publisher = publisher
-
+        self.period = period
 
 b1 = Book("Brave New World", "Aldous Huxley", 311, 29.0)
 n1 = Newspaper("NY Times", "New York Times Company", 6.0, "Daily")
 m1 = Magazine("Scientific American", "Springer Nature", 5.99, "Monthly")
 
-print(b1.author)
-print(n1.publisher)
-print(b1.price, m1.price, n1.price)
+print(b1.author)     # Aldous Huxley
+print(n1.publisher)  # New York Times Company
+print(b1.price, m1.price, n1.price)  # 29.0 5.99 6.0
